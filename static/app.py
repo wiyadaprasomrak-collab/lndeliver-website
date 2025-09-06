7271853feca1aeb2882f200aa744012c5baccec1
wiyadaprasomrak-collab <wiyadaprasomrak@gmail.com>
2025-09-06 03:08:19 +0000

    Fixed TemplateNotFound error

--- static/app.py
+++ static/app.py
@@ -2,7 +2,11 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import datetime
 
-app = Flask(__name__)
+# แก้ไขบรรทัดนี้เพื่อระบุพาธของโฟลเดอร์ template และ static อย่างชัดเจน
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 app.secret_key = os.urandom(24)
 
 AUTHORIZED_EMAIL = "wiyadaprasomrak@gmail.com"
